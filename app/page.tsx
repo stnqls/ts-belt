4b00db742d00c6efdae10199496d89843fe7a8d6
stnqls <stnqls3938@gmail.com>
2026-01-19 21:41:38 +0900

    feat: 메인페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <Link href={"/ts-belt"}>ts-belt</Link>
       </div>
+
       <div>리베이스 테스트 해보기</div>
+
+      <p>리베이스 어려움</p>
     </div>
   );
 }
